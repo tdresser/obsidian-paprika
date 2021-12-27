00027d5e79ce0586ae1775491ec9cb8a6ac2d522
Tim <tdresser@chromium.org>
2021-12-27 17:20:31 -0500

    Add binary for testing

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -19,12 +19,24 @@ pub fn greet() {
     alert("Hello, rust!");
 }
 
-pub async fn paprika_login(email: String, password: String) -> String {
-    let res = api::login(&email, &password).await;
+pub async fn paprika_login(email: &String, password: &String) -> String {
+    let res = api::login(email, password).await;
     match res {
         Ok(t) => {
             return t.token;
         }
         Err(e) => unreachable!(e),
+    }
+}
+
+async fn list_recipes(token: &String) {
+    let recipe_list = api::get_recipes(&token).await.unwrap();
+
+    for (_, recipe_entry) in recipe_list.iter().enumerate() {
+        let recipe_future = api::get_recipe_by_id(&token, &recipe_entry.uid).await;
+        match recipe_future {
+            Ok(recipe) => println!("Recipe: {:?}", recipe),
+            Err(e) => println!("Error fetching recipe {}: {}", recipe_entry.uid, e),
+        }
     }
 }
