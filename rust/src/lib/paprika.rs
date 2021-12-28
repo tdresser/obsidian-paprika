6cd92b6ae85abe5d66c0fee0bdaa61bf376d5a3b
Tim <tdresser@chromium.org>
2021-12-28 14:20:35 -0500

    Categories mapped to strings.

--- rust/src/lib/paprika.rs
+++ rust/src/lib/paprika.rs
@@ -1,13 +1,14 @@
 use handlebars::Handlebars;
 use paprika_api::api::{self, Category, Recipe, RecipeEntry};
+use serde_json::Value;
 
 use crate::lib::handlebars_helpers::newlines_to_bullets;
 
-fn get_category_strings(recipe: Recipe, categories: &Vec<Category>) -> Vec<String> {
+fn get_category_strings(recipe: &Recipe, categories: &Vec<Category>) -> Vec<String> {
     let mut s:Vec<String> = Vec::new();
-    for recipe_category_uid in recipe.categories {
+    for recipe_category_uid in &recipe.categories {
         for category in categories {
-            if recipe_category_uid == category.uid {
+            if *recipe_category_uid == category.uid {
                 s.push(category.name.clone());
             }
         }
@@ -24,8 +25,18 @@ pub fn get_markdown(
     handlebars.set_strict_mode(true);
     handlebars.register_helper("newlines_to_bullets", Box::new(newlines_to_bullets));
 
+    let mut recipe_value = serde_json::to_value(recipe).unwrap();
+
+    let category_strings = get_category_strings(recipe, categories);
+    let categories_value = serde_json::to_value(category_strings).unwrap();
+    
+    if let Value::Object(o) = &mut recipe_value {
+        println!("DOING IT!");
+        o.insert("categories".to_string(), categories_value);
+    }
+
     assert!(handlebars
         .register_template_string("template", template)
         .is_ok());
-    return handlebars.render("template", &recipe).unwrap();
+    return handlebars.render("template", &recipe_value).unwrap();
 }
