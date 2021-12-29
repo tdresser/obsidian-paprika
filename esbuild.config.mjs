f8466b2f59ba5607230386c6bd8bb50da446542c
Tim <tdresser@chromium.org>
2021-12-29 16:37:15 -0500

    Building wasm

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -6,7 +6,7 @@ import wasmLoader from 'esbuild-plugin-wasm';
 const prod = (process.argv[2] === 'production');
 
 esbuild.build({
-	plugins: [wasmLoader()],
+	plugins: [wasmLoader.wasmLoader()],
 	entryPoints: ['main.ts', 'local.ts'],
 	bundle: true,
 	external: ['obsidian', 'electron', ...builtins],
